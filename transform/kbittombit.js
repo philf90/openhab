cdb5728edd3829cb0bb40c6be182bc97a3d40371
Philipp <35610275+philf90@users.noreply.github.com>
2021-01-23 10:04:31 +0100

    Add files via upload

--- transform/kbittombit.js
+++ transform/kbittombit.js
@@ -1,5 +1,5 @@
 //convert Kilobit to Megabit
 (function(i){ 
-	var result = ((parseInt(i) / 1000).toFixed(3) + " MBit/s");
+	var result = ((parseInt(i) / 1000000).toFixed(3) + " MBit/s");
 	return result;
 })(input)
